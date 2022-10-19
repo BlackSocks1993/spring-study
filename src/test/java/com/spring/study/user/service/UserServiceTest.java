7de3a14dd51ccaabee3a56329cfda2cab62e37da
GwangHyeok <blacksocks1993@gmail.com>
2022-10-19 21:24:47 +0900

    feat

--- src/test/java/com/spring/study/user/service/UserServiceTest.java
+++ src/test/java/com/spring/study/user/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Before;
@@ -18,13 +17,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.spring.study.user.dao.UserDao;
 import com.spring.study.user.domain.Level;
@@ -34,6 +36,11 @@ import com.spring.study.user.domain.User;
 @ContextConfiguration(locations={
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+//@Transactional
+/* 롤백 여부에 대한 기본 설정과 매니저 빈을 지정하는 데 사용할 수 있다.
+ * 디폴트 트랜잭션 매니저 아이디는 관례를 따라서 transactionManager로 되어 있다.
+ */
+// @TransactionConfiguration(defaultRollback=false)
 public class UserServiceTest {
 
 	// 팩토리 빈을 가져오려면 애플리케이션 컨텍스트가 필요하다.
@@ -58,11 +65,13 @@ public class UserServiceTest {
 		);
 	}
 
-	@Test 
+	@Test
+	// 롤백 여부애 대한 기본 설정이 false인 경우 해당 어노테이션을 사용해서 롤백 여부를 재설정 할 수 있다.
+	// @Rollback
 	public void upgradeLevels() throws Exception {
-		UserServiceImpl userServiceImpl = new UserServiceImpl(); 
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
 
-		MockUserDao mockUserDao = new MockUserDao(this.users);  
+		MockUserDao mockUserDao = new MockUserDao(this.users);
 		userServiceImpl.setUserDao(mockUserDao);
 
 		MockMailSender mockMailSender = new MockMailSender();
@@ -70,9 +79,9 @@ public class UserServiceTest {
 
 		userServiceImpl.upgradeLevels();
 
-		List<User> updated = mockUserDao.getUpdated();  
-		assertThat(updated.size(), is(2));  
-		checkUserAndLevel(updated.get(0), "admin02", Level.SILVER); 
+		List<User> updated = mockUserDao.getUpdated();
+		assertThat(updated.size(), is(2));
+		checkUserAndLevel(updated.get(0), "admin02", Level.SILVER);
 		checkUserAndLevel(updated.get(1), "user02", Level.GOLD);
 
 		List<String> request = mockMailSender.getRequests();
@@ -87,8 +96,8 @@ public class UserServiceTest {
 	}
 
 	static class MockUserDao implements UserDao { 
-		private List<User> users;  
-		private List<User> updated = new ArrayList(); 
+		private List<User> users;
+		private List<User> updated = new ArrayList();
 
 		private MockUserDao(List<User> users) {
 			this.users = users;
@@ -113,7 +122,7 @@ public class UserServiceTest {
 	}
 
 	static class MockMailSender implements MailSender {
-		private List<String> requests = new ArrayList<String>();	
+		private List<String> requests = new ArrayList<String>();
 		
 		public List<String> getRequests() {
 			return requests;
@@ -121,7 +130,7 @@ public class UserServiceTest {
 
 		@Override
 		public void send(SimpleMailMessage mailMessage) throws MailException {
-			requests.add(mailMessage.getTo()[0]);  
+			requests.add(mailMessage.getTo()[0]);
 		}
 
 		@Override
@@ -130,14 +139,17 @@ public class UserServiceTest {
 	}
 
 	@Test
+	// @Transactional
+	// @Transactional(readOnly=true)
+	// @Rollback(false)
 	public void mockUpgradeLevels() throws Exception {
 		UserServiceImpl userServiceImpl = new UserServiceImpl();
 
-		UserDao mockUserDao = mock(UserDao.class);	    
+		UserDao mockUserDao = mock(UserDao.class);
 		when(mockUserDao.getAll()).thenReturn(this.users);
 		userServiceImpl.setUserDao(mockUserDao);
 
-		MailSender mockMailSender = mock(MailSender.class);  
+		MailSender mockMailSender = mock(MailSender.class);
 		userServiceImpl.setMailSender(mockMailSender);
 
 		userServiceImpl.upgradeLevels();
@@ -149,7 +161,7 @@ public class UserServiceTest {
 //		verify(mockUserDao).update(users.get(3));
 //		assertThat(users.get(3).getLevel(), is(Level.GOLD));
 //
-//		ArgumentCaptor<SimpleMailMessage> mailMessageArg = ArgumentCaptor.forClass(SimpleMailMessage.class);  
+//		ArgumentCaptor<SimpleMailMessage> mailMessageArg = ArgumentCaptor.forClass(SimpleMailMessage.class);
 //		verify(mockMailSender, times(2)).send(mailMessageArg.capture());
 //		List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues();
 //		assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail()));
@@ -181,7 +193,7 @@ public class UserServiceTest {
 		User userWithLevelRead = userDao.get(userWithLevel.getId());
 		User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
 
-		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel())); 
+		assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
 		assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
 	}
 
@@ -208,7 +220,7 @@ public class UserServiceTest {
 
 		try {
 			this.testUserService.upgradeLevels();
-			fail("TestUserServiceException expected"); 
+			fail("TestUserServiceException expected");
 		}
 		catch(TestUserServiceException e) {}
 
@@ -227,17 +239,18 @@ public class UserServiceTest {
 	 * 포인트컷의 클래스 필터에 선정되도록 이름을 변경한다.
 	 * 이래서 처음부터 이름을 잘 지어야 한다.
 	 */
-	static class TestUserServiceImpl extends UserServiceImpl {
+	static class TestUserService extends UserServiceImpl {
 		// 테스트 픽스처의 users(3)의 id 값을 고정시켜버렸다.
 		private String id = "user02";
-		
+
 //		private TestUserService(String id) { this.id = id; }
 
 		// 읽기전용 트랜잭션 대상인 get을 오버라이드 한다.
 		@Override
-		public List<User> getAll() {
+		public List<User> getAll() {	
 			for (User user : super.getAll()) {
 				// 강제로 쓰기 시도를 한다.
+				//super.update(user);
 				super.update(user);
 			}
 			// 메소드가 끝나기 전에 예외가 발생해야 하니 리턴 값은 적당히 넣는다.
@@ -246,11 +259,25 @@ public class UserServiceTest {
 
 		@Override
 		protected void upgradeLevel(User user) {
-			if (user.getId().equals(this.id)) throw new TestUserServiceException();  
+			if (user.getId().equals(this.id)) throw new TestUserServiceException();
 			super.upgradeLevel(user);
 		}
 	}
 
 	static class TestUserServiceException extends RuntimeException {}
+
+	@Test
+	public void transactionSync() {
+		DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
+		txDefinition.setReadOnly(true);
+		TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
+
+		userService.deleteAll();
+
+		userService.add(users.get(0));
+		userService.add(users.get(1));
+
+		transactionManager.commit(txStatus);
+	}
 
 }
